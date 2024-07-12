8a1735e9a8168a5e2c62718d84ef1b094a6108b6
Akshay V Anil <30944903+akshay117@users.noreply.github.com>
2024-07-12 21:55:20 +0530

    first:commit

---
+++ src/main/java/com/ghostify/files/FirstCicdApplication.java
@@ -0,0 +1,13 @@
+package com.ghostify.files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghostify/files/FirstCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostify.files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
